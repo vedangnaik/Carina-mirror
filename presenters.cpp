83afda1a91c923ca33dfea08359d2aac0ad842c0
Vedang Naik <vedang.a.naik@gmail.com>
2020-11-05 18:59:03 +0530

    Finished state display function

--- presenters.cpp
+++ presenters.cpp
@@ -1,5 +1,9 @@
 #include "presenters.h"
 
 void ProcessPresenter::displayState(State* s) {
+    bool proceed = true;
+    bool abort = false;
+    if (s->proceedState == "") { proceed = false; }
+    if (s->abortState == "") { abort = false; }
     this->ppoc->displayState(s);
 }
--- userinterface.cpp
+++ userinterface.cpp
@@ -4,6 +4,7 @@
 #include <QFileDialog>
 #include <QtUiTools>
 #include <QHBoxLayout>
+#include <iostream>
 
 UserInterface::UserInterface(PCIC* pcic, QWidget *parent) : QMainWindow(parent) , ui(new Ui::GSUI) {
     ui->setupUi(this);
@@ -16,6 +17,8 @@ UserInterface::UserInterface(PCIC* pcic, QWidget *parent) : QMainWindow(parent) 
             this->pcic->openProcess(fileName.toStdString());
         }
     });
+
+    connect(ui->proceedButton, &QPushButton::clicked, this->pcic, &PCIC::proceed);
 }
 
 UserInterface::~UserInterface() {
@@ -23,11 +26,24 @@ UserInterface::~UserInterface() {
 }
 
 void UserInterface::displayState(State* s) {
+    QLayoutItem* child;
+    while ((child = ui->currentStateLayout->takeAt(0)) != nullptr) {
+        delete child->widget();
+        delete child;
+    }
+
     QFile file("currentState.ui");
     file.open(QIODevice::ReadOnly);
     QUiLoader loader;
+    QWidget* stateFrame = loader.load(&file, this);
+    file.close();
 
-    QWidget* currentStateWidget = loader.load(&file, this);
-    delete ui->currentStateLayout->widget();
-    ui->currentStateLayout->addWidget(currentStateWidget);
+    QLabel* nameLabel = findChild<QLabel*>("nameLabel");
+    nameLabel->setText(QString::fromStdString(s->name));
+    QLabel* descriptionLabel = findChild<QLabel*>("descriptionLabel");
+    descriptionLabel->setText(QString::fromStdString(s->description));
+    QLabel* abortsToLabel = findChild<QLabel*>("abortsToLabel");
+    abortsToLabel->setText(QString::fromStdString(s->abortState));
+
+    ui->currentStateLayout->addWidget(stateFrame);
 }
