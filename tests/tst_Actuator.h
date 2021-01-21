84975e0593e061c9e26004ce1f545a18e3618787
Vedang Naik <vedang.a.naik@gmail.com>
2021-01-21 13:24:31 +0530

    Fixed unit tests

--- tests/tst_Actuator.h
+++ tests/tst_Actuator.h
@@ -6,25 +6,18 @@
 
 class ActuatorTest : public ::testing::Test {
 protected:
-    void SetUp() override {
-        std::string id = "act1";
-        std::string name = "act1name";
-        this->a = std::make_unique<Actuator>(id, name);
-        ASSERT_NE(this->a, nullptr);
-    }
-
-    std::unique_ptr<Actuator> a;
+    Actuator a = Actuator("act1", "act1name");
 };
 
 TEST_F(ActuatorTest, BasicProperties) {
-    ASSERT_EQ(a->id, "act1");
-    EXPECT_EQ(a->name, "act1name");
-    ASSERT_EQ(a->status, false);
+    ASSERT_EQ(a.id, "act1");
+    EXPECT_EQ(a.name, "act1name");
+    ASSERT_EQ(a.status, false);
 }
 
 TEST_F(ActuatorTest, ChangingStatus) {
-    this->a->status = true;
-    ASSERT_EQ(this->a->status, true);
-    this->a->status = false;
-    ASSERT_EQ(this->a->status, false);
+    this->a.status = true;
+    ASSERT_EQ(this->a.status, true);
+    this->a.status = false;
+    ASSERT_EQ(this->a.status, false);
 }
