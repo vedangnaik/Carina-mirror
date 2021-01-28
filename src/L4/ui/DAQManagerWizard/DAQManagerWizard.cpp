6f602f6d401b083e9d3ba69ccfb7c6df2506bc02
Vedang Naik <vedang.a.naik@gmail.com>
2021-01-28 13:00:02 +0530

    Added linker parsing code

--- src/L4/ui/DAQManagerWizard/DAQManagerWizard.cpp
+++ src/L4/ui/DAQManagerWizard/DAQManagerWizard.cpp
@@ -26,13 +26,23 @@ DAQManagerWizard::accept()
         if (id.find("dummy") != std::string::npos) {
             unsigned int numChannels = this->field(QString::fromStdString(id + "|numChannels")).toUInt();
             const auto& calibrationPoints = dcp->calibrationPoints.at(id);
-            //DAQDevices.push_back(new DummyDAQ(id, numChannels, calibrationPoints));
+            DAQDevices.push_back(new DummyDAQ(id, numChannels, calibrationPoints));
         }
     }
 
     // assemble map of links
+    std::map<std::string, std::pair<AbstractDAQ*, unsigned int>> sensorToDAQLinks;
     for (const auto& p : dlp->sensorLinks) {
-        LOG(INFO) << p.first << ": " << p.second;
+        const auto& IDAndChannel = p.second;
+        std::string::size_type n = IDAndChannel.find("-");
+        std::string id = IDAndChannel.substr(0, n);
+        unsigned int numChannels = std::stoul(IDAndChannel.substr(n, IDAndChannel.length()));
+        LOG(INFO) << id << ": " << numChannels;
+
+        AbstractDAQ* daq = *std::find_if(DAQDevices.begin(), DAQDevices.end(), [=](AbstractDAQ* d) {
+            return d->deviceID == id;
+        });
+        sensorToDAQLinks.insert({ p.first, std::make_pair(daq, numChannels) });
     }
 
 
