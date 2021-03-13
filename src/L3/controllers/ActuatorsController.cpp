76b305273010bc15a96841146990f87f009cda32
Vedang Naik <vedang.a.naik@gmail.com>
2021-03-14 03:43:48 +0530

    Removed unnecessary actuator status logging code from 12398123 years ago

--- src/L3/controllers/ActuatorsController.cpp
+++ src/L3/controllers/ActuatorsController.cpp
@@ -2,6 +2,4 @@
 
 void ActuatorsController::actuate(std::string id) {
     this->amic.actuate(id);
-    ActuatorStatusGateway* asg = new ActuatorStatusGateway("actuatorStatuses.json");
-    asg->updateActutatorStatus(id, this->amic.getActuatorStatus(id));
 }
