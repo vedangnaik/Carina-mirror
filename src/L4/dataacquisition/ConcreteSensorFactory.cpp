2d12006eb8b7f0cb31b223d03f2562760f56a942
Vedang Naik <vedang.a.naik@gmail.com>
2021-08-17 12:42:41 +0530

    Making implementation of concrete sensor factory more extensible

--- src/L4/dataacquisition/ConcreteSensorFactory.cpp
+++ src/L4/dataacquisition/ConcreteSensorFactory.cpp
@@ -1,26 +1,21 @@
 #include "ConcreteSensorFactory.h"
 
+std::unordered_map<std::string, Sensor* (*)(const std::string&, const QVariantMap&)> ConcreteSensorFactory::factoryMap = {
+    {"DummySensor", &ConcreteSensorFactory::createDummySensor},
+    {"AnalogMCCDAQSensor", &ConcreteSensorFactory::createAnalogMCCDAQSensor},
+};
+
 Sensor* ConcreteSensorFactory::createSensor(const std::string& id, const QVariantMap& args) {
-    // TODO: Check for required keys and throw exceptions as needed.
-    if (args["type"] == "DummySensor") {
-        return new DummySensor(id, parseCalibrationPointsFromArgs(args));
+    if (!args.contains("type")) {
+        throw std::domain_error(id + ": Sensor must have a type.");
     }
-    else if (args["type"] == "AnalogMCCDAQSensor") {
-#ifdef ULDAQ_AVAILABLE
-        // connect DAQ
-        DaqDeviceHandle handle = (DaqDeviceHandle)args["handle"].toLongLong();
-        UlError err = ulConnectDaqDevice(handle);
-        if (err != ERR_NO_ERROR) { LOG(ERROR) << "ulConnectDaqDevice Error: " << err; }
-        return new AnalogMCCDAQSensor(id, parseCalibrationPointsFromArgs(args), args["channel"].toUInt(), handle);
-#else
-        LOG(WARNING) << "MCCDAQs are not supported for this platform."
-        // TODO: Exception
-#endif
+
+    std::string type = args["type"].toString().toStdString();
+    if (factoryMap.count(type) == 0) {
+        throw std::domain_error(id + ": Type '" + type + "' is invalid or not supported.");
     }
-    else {
-        // TODO: Exception
-        return nullptr;
-    }
+
+    return factoryMap[type](id, args);
 }
 
 std::array<std::pair<double, double>, 5>
@@ -34,4 +29,39 @@ ConcreteSensorFactory::parseCalibrationPointsFromArgs(const QVariantMap &args) {
         calibrationPoints.at(i) = t;
     }
     return calibrationPoints;
+}
+
+Sensor* ConcreteSensorFactory::createDummySensor(const std::string &id, const QVariantMap &args) {
+    return new DummySensor(id, parseCalibrationPointsFromArgs(args));
+}
+
+Sensor* ConcreteSensorFactory::createAnalogMCCDAQSensor(const std::string &id, const QVariantMap &args) {
+#ifdef ULDAQ_AVAILABLE
+    // Retrieve the handle
+    checkForKeyAndConversionValidity(args, "handle", QMetaType::LongLong, id + ": Sensor must contain a valid numeric MCC device handle.");
+    DaqDeviceHandle handle = (DaqDeviceHandle)args["handle"].toLongLong();
+
+    // Retrieve the channel this is connected to
+
+    // If this DAQ has already been connected, leave it. Connecting twice appears to actually disconnect the DAQ xD
+    int isConnected;
+    ulIsDaqDeviceConnected(handle, &isConnected);
+    if (isConnected == 0) {
+        UlError err = ulConnectDaqDevice(handle);
+        if (err != ERR_NO_ERROR) {
+            throw std::runtime_error(id + ": Unable to connect to MCC device with handle '" + std::to_string(handle) + "'.");
+        }
+    }
+    return new AnalogMCCDAQSensor(id, parseCalibrationPointsFromArgs(args), args["channel"].toUInt(), handle);
+#else
+    throw std::domain_error(id + ": This Carina has not been compiled to support AnalogMCCDAQSensors. Please consult the developers for further information.")
+#endif
+
+}
+
+void
+ConcreteSensorFactory::checkForKeyAndConversionValidity(const QVariantMap& args, const QString& key, int targetTypeId, const std::string& exceptionMsg) {
+    if (!args.contains(key) || !args[key].canConvert(targetTypeId)) {
+        throw std::domain_error(exceptionMsg);
+    }
 }
--- src/L4/dataacquisition/ConcreteSensorFactory.h
+++ src/L4/dataacquisition/ConcreteSensorFactory.h
@@ -2,6 +2,7 @@
 
 #include <QVariantMap>
 #include <QJsonArray>
+#include <unordered_map>
 #include "easylogging++.h"
 #include "Sensor.h"
 #include "DummySensor.h"
@@ -15,4 +16,10 @@ public:
     static Sensor* createSensor(const std::string& id, const QVariantMap& args);
 private:
     static std::array<std::pair<double, double>, 5> parseCalibrationPointsFromArgs(const QVariantMap& args);
+    static Sensor* createDummySensor(const std::string& id, const QVariantMap& args);
+    static Sensor* createAnalogMCCDAQSensor(const std::string& id, const QVariantMap& args);
+
+    static void checkForKeyAndConversionValidity(const QVariantMap& args, const QString& key, int targetTypeId, const std::string& exceptionMsg);
+
+    static std::unordered_map<std::string, Sensor* (*)(const std::string&, const QVariantMap&)> factoryMap;
 };
