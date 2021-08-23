fd92586e9ee595f9ef6f8f19192f93eaa0d8cd4b
Vedang Naik <vedang.a.naik@gmail.com>
2021-08-23 13:16:26 +0530

    Made changes to UI to accommodate variable calibration points, fixed bug in recalibration

--- src/L4/ui/RecalibrationWindow.cpp
+++ src/L4/ui/RecalibrationWindow.cpp
@@ -8,16 +8,14 @@ RecalibrationWindow::RecalibrationWindow(Sensor* s, QWidget *parent) :
     // Connect dialog buttons
     connect(ui->buttonBox, &QDialogButtonBox::accepted, this, &QDialog::accept);
     connect(ui->buttonBox, &QDialogButtonBox::rejected, this, &QDialog::reject);
-    // Copy calibration values into line edits
+    // Create line edits for each set of calibration points and fill them.
     QFormLayout* f = (QFormLayout*)this->ui->pointsWidget->layout();
     // The rows are numbered from 1 to 5 instead of 0 to 4, so we have two counters in here
-    for (int row = 1, i = 0; row <= 5; row++, i++) {
-        QLineEdit* rawValue = (QLineEdit*)f->itemAt(row, QFormLayout::LabelRole)->widget();
-        rawValue->setText(QString::number(s->calibrationPoints.at(i).first));
-        QLineEdit* unitsValue = (QLineEdit*)f->itemAt(row, QFormLayout::FieldRole)->widget();
-        unitsValue->setText(QString::number(s->calibrationPoints.at(i).second));
+    for (const auto& point : s->calibrationPoints) {
+        QLineEdit* rawValue = new QLineEdit(QString::number(point.first));
+        QLineEdit* unitsValue = new QLineEdit(QString::number(point.second));
+        f->addRow(rawValue, unitsValue);
     }
-
 }
 
 RecalibrationWindow::~RecalibrationWindow()
@@ -26,14 +24,16 @@ RecalibrationWindow::~RecalibrationWindow()
 }
 
 void RecalibrationWindow::accept() {
-    // Copy line edit values back into calibration points, then recalibrate sensor
+    // Clear the existing calibration points.
+    this->sensorToRecalibrate->calibrationPoints.clear();
+    // Copy line edit values back into calibration points, then recalibrate sensor.
     QFormLayout* f = (QFormLayout*)this->ui->pointsWidget->layout();
-    // The rows are numbered from 1 to 5 instead of 0 to 4, so we have two counters in here
-    for (int row = 1, i = 0; row <= 5; row++, i++) {
+    // We skip the first row since that's the header.
+    for (int row = 1; row < f->rowCount(); row++) {
         QLineEdit* rawValue = (QLineEdit*)f->itemAt(row, QFormLayout::LabelRole)->widget();
-        this->sensorToRecalibrate->calibrationPoints.at(i).first = rawValue->text().toDouble();
         QLineEdit* unitsValue = (QLineEdit*)f->itemAt(row, QFormLayout::FieldRole)->widget();
-        this->sensorToRecalibrate->calibrationPoints.at(i).second = unitsValue->text().toDouble();
+        std::pair<double, double> t {rawValue->text().toDouble(), unitsValue->text().toDouble()};
+        this->sensorToRecalibrate->calibrationPoints.push_back(t);
     }
     this->sensorToRecalibrate->calibrate();
     this->done(QDialog::Accepted);
