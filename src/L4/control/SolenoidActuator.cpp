296428bc132ca45260ce4258ec358ef7ebec6689
Hamza Dugmag <hdugmag@gmail.com>
2021-09-04 18:30:30 -0400

    Manage solenoid state

--- src/L4/control/SolenoidActuator.cpp
+++ src/L4/control/SolenoidActuator.cpp
@@ -2,11 +2,26 @@
 
 #include "SolenoidActuator.h"
 
-SolenoidActuator::SolenoidActuator(std::string id, uint8_t pin)
+SolenoidActuator::SolenoidActuator(std::string id, uint8_t relayChannel, uint8_t gpioPin, bool nominallyPowered)
     : Actuator(id)
 {}
 
-void DummyActuator::setState(const bool state) {
+void SolenoidActuator::setState(const bool state) {
+    this->state = state;
+    if (this->state == nominallyPowered) {
+        LOG(INFO) << "SolenoidActuator id '" << this->id << "' is now turned 'OFF'.";
+    } else {
+        LOG(INFO) << "SolenoidActuator id '" << this->id << "' is now turned 'ON'.";
+    }
+
+    if (this->state) {
+        setDigitalPin(gpioPin, false);
+    } else {
+        setDigitalPin(gpioPin, true);
+    }
+}
+
+void SolenoidActuator::setDigitalPin(uint8_t pin, bool turnRelayChannelOn) {
     //
 }
 
